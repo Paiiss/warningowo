4431a252760018fc365e9bd25cdf90e715cadaa7
Paiss <pais.2k05@gmail.com>
2024-09-27 00:56:00 +0700

    Refactor gitignore and setActivity in index.js

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ client.on("ready", () => {
   const getActive = JSON.parse(fs.readFileSync("./data/active.json"));
   active_user.push(...getActive);
 
-  client.user.setActivity("OwO", { type: "WATCHING" });
+  client.user.setActivity({
+    name: "OwO",
+    type: "WATCHING",
+  });
 
   setInterval(() => {
     fs.writeFileSync("./data/active.json", JSON.stringify(active_user));
@@ -32,20 +35,20 @@ client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
+    return interaction.reply("Pong!");
   }
 
   if (interaction.commandName === "alert") {
     if (interaction.options.get("type").value) {
       if (active_user.includes(interaction.user.id))
-        interaction.reply("You are already active");
+        return interaction.reply("You are already active");
       active_user.push(interaction.user.id);
       return interaction.reply(
         "We will remind you when OwO asks for a captcha"
       );
     } else {
       if (!active_user.includes(interaction.user.id))
-        interaction.reply("You are already inactive");
+        return interaction.reply("You are already inactive");
       const index = active_user.indexOf(interaction.user.id);
       active_user.splice(index, 1);
 
@@ -58,22 +61,19 @@ client.on("interactionCreate", async (interaction) => {
 
 client.on("messageCreate", async (message) => {
   if (message.author.id != "408785106942164992") return;
-  if (active_user.includes(message.mentions.users.first().id)) {
-    if (
-      message.content.match(
-        /human|captcha|dm|banned|https:\/\/owobot.com\/captcha/g
-      )
-    ) {
-      const user = await client.users.fetch(message.mentions.users.first());
-      if (!user) {
-        console.log("User not found");
-        return;
-      }
-
-      return user.send(
-        "Hello, It looks like you were detected as a bot by OwO."
-      );
+  if (
+    message.content.match(
+      /human|captcha|dm|banned|https:\/\/owobot.com\/captcha/g
+    ) &&
+    active_user.includes(message.mentions.users.first().id)
+  ) {
+    const user = await client.users.fetch(message.mentions.users.first());
+    if (!user) {
+      console.log("User not found");
+      return;
     }
+
+    return user.send("Hello, It looks like you were detected as a bot by OwO.");
   }
 });
 
